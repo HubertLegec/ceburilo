eab3a2dc0d4ae452abbff93330077c3bb22f5829
HubertLegec <hubert.legec@gmail.com>
2017-05-27 20:04:19 +0200

    time counter

--- app/src/main/kotlin/com/legec/ceburilo/MapsActivity.kt
+++ app/src/main/kotlin/com/legec/ceburilo/MapsActivity.kt
@@ -1,15 +1,19 @@
 package com.legec.ceburilo
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import butterknife.BindString
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,6 +21,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PolylineOptions
 import com.legec.ceburilo.utils.MapElements
+import com.legec.ceburilo.utils.SharedPrefsService
 import com.legec.ceburilo.web.directions.GoogleDirectionsService
 import com.legec.ceburilo.web.directions.RouteCallback
 import com.legec.ceburilo.web.maps.GoogleLocationService
@@ -31,19 +36,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var startPointIdDescriptor: String
     @BindString(R.string.end_point_id)
     lateinit var endPointIdDescriptor: String
+    @BindString(R.string.dystans)
+    lateinit var distanceText: String
+    @BindString(R.string.czas_przejazdu)
+    lateinit var timeText: String
+    @BindString(R.string.time_left)
+    lateinit var timeLeftText: String
     @BindView(R.id.distanceTV)
     lateinit var distanceTV: TextView
     @BindView(R.id.durationTV)
     lateinit var durationTV: TextView
     @BindView(R.id.progressbar_view)
     lateinit var progressbarView: LinearLayout
+    @BindView(R.id.startStopButton)
+    lateinit var startStopButton: Button
+    @BindView(R.id.timeLeftTV)
+    lateinit var timeLeftTV: TextView
     @Inject lateinit var googleLocationService: GoogleLocationService
     @Inject lateinit var veturiloApiService: VeturiloApiService
     @Inject lateinit var googleDirectionsService: GoogleDirectionsService
+    @Inject lateinit var sharedPrefsService: SharedPrefsService
     private var mMap: GoogleMap? = null
     private var startPoint: VeturiloPlace? = null
     private var endPoint: VeturiloPlace? = null
     private var mapElements: MapElements? = null
+    private var timer: CountDownTimer? = null
 
     /**
      * Get route callback.
@@ -53,8 +70,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val callback = object: RouteCallback {
         override fun onSuccess(polylines: List<PolylineOptions>, distance: String, duration: String, waypoints: List<VeturiloPlace>) {
             updateMap(polylines, waypoints)
-            distanceTV.text = "Dystans: " + distance
-            durationTV.text = "Czas przejazdu: " + duration
+            distanceTV.text = distanceText + " " + distance
+            durationTV.text = timeText + " " + duration
+            timeLeftTV.text = timeLeftText + " " + sharedPrefsService.getRoutePartMaxTime() + ":00"
             progressbarView.visibility = View.GONE
             Log.i(TAG, "Success")
         }
@@ -69,15 +87,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         ButterKnife.bind(this)
         CeburiloApp.appComponent.inject(this)
         updateStartAndEndPoint()
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    @OnClick(R.id.startStopButton)
+    fun onStartTimeClick() {
+        if (startStopButton.text == "Reset") {
+            timer?.cancel()
+            startStopButton.setTextColor(resources.getColor(R.color.colorPrimaryDark))
+        }
+
+        val interval = 1000
+        val milisInFuture = sharedPrefsService.getRoutePartMaxTime() * 60 * 1000
+        timer = object: CountDownTimer(milisInFuture.toLong(), interval.toLong()) {
+            override fun onFinish() {
+                timeLeftTV.text = "Koniec czasu!"
+                timeLeftTV.setTextColor(Color.RED)
+                startStopButton.text = "Start"
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsTotal = millisUntilFinished / 1000
+                val minutes = secondsTotal / 60
+                val seconds = secondsTotal - minutes * 60
+                timeLeftTV.text = timeLeftText + " " + minutes.toString() + ":" + seconds.toString()
+            }
+
+        }
+        timer?.start()
+        startStopButton.text = "Reset"
+    }
 
     /**
      * Manipulates the map once available.
